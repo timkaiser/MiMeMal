83fe9dffcb95af032d22f3ccbe70534222d156f7
Jonathan <jjjooonnn@gmail.com>
2019-06-21 18:43:32 +0200

    Main menu, info display
    Added a functional main menu, info menu with prototype info buttons.

---
+++ Assets/Resources/Scripts/sc_InfoButtons.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class sc_InfoButtons : MonoBehaviour
+{
+
+    public GameObject textContainer;
+
+    public GameObject modelInfo;
+
+    public GameObject otherInfo;
+
+    public void DisplayInfo(int index) {
+        if (!textContainer.activeSelf)
+            textContainer.SetActive(true);
+        Clear();
+        textContainer.transform.GetChild(index).gameObject.SetActive(true);
+    }
+
+    public void Exit() {
+        EditorSceneManager.LoadScene(0);
+    }
+
+    void Clear() {
+        for(int i = 0; i < textContainer.transform.childCount; i++){
+            textContainer.transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+
+}
