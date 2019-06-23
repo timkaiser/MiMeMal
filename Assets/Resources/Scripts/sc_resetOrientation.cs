6d430e8cd9bc08de6c48d3a068b7c5a754186f1c
Jonathan <jjjooonnn@gmail.com>
2019-06-23 22:57:53 +0200

    Gallery
    Added a gallery, featuring a statue which is rotatable. rotation is resettable via button.
    TODOS: configure that the statue is textured according to saved textures

---
+++ Assets/Resources/Scripts/sc_resetOrientation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_resetOrientation : MonoBehaviour
+{
+    public GameObject resetee;
+
+    public void resetOrientation() {
+        resetee.transform.rotation = Quaternion.identity;
+    }
+}
