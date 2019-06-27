1c80cf42d76bbbaa878d3c8e38d53028948b0ab6
Annalena <bloch@in.tum.de>
2019-06-27 09:08:43 +0200

    finished color picker

--- Assets/Resources/Scripts/sc_colorPicker.cs
+++ Assets/Resources/Scripts/sc_colorPicker.cs
@@ -7,33 +7,51 @@ using UnityEngine.UI;
 public class sc_colorPicker : MonoBehaviour
 {
     public Slider valueSlider;
-    public Color currentColor = Color.white;
-    [Range(0f, 1f)]
-    private float value = 1f;
+    public RawImage pointer;
+    public RawImage displayedColor;
+
+    private Vector3 currentColor = new Vector3(0, 0, 1);
+
+    public void OnApplicationQuit()
+    {
+        //Reset to white
+        valueSlider.transform.Find("Color").GetComponent<CanvasRenderer>().GetMaterial()
+            .SetVector("_HSVColor", new Vector4(0, 0, 1, 1));
+        gameObject.GetComponent<CanvasRenderer>().GetMaterial().SetFloat("_Value", 1f);
+    }
 
     public void OnColorPickerClicked()
     {
+        //Get relative position
         RectTransform boundingRect = GetComponent<RectTransform>();
         float x = (Input.mousePosition.x - boundingRect.position.x) / boundingRect.rect.size.x * 2;
         float y = (Input.mousePosition.y - boundingRect.position.y) / boundingRect.rect.size.y * 2;
 
-        //compute polar coords
+        //Compute polar coords
         float saturation = Mathf.Sqrt((x * x) + (y * y));
         float hue = (Mathf.Atan2(y, x) / (2*Mathf.PI)) + 0.5f;
 
-        //if outside cicle
+        //If outside cicle neglect
         if (saturation > 1) return;
 
-        currentColor = Color.HSVToRGB(hue, saturation, value);
-
+        //Communicate color to slider shader
         valueSlider.transform.Find("Color")
                   .GetComponent<CanvasRenderer>().GetMaterial().SetVector("_HSVColor",
-                  new Vector4(hue, saturation, value, 1));
+                  new Vector4(hue, saturation, currentColor.z, 1));
+        //Move pointer
+        pointer.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+        //Set currently displayed color
+        currentColor = new Vector3(hue, saturation, currentColor.z);
+        Color current = Color.HSVToRGB(hue, saturation, currentColor.z);
+        displayedColor.GetComponent<CanvasRenderer>().SetColor(current);
     }
     public void OnValueSliderChanged()
     {
-        this.value = valueSlider.value;
-        Debug.Log(value);
-        gameObject.GetComponent<CanvasRenderer>().GetMaterial().SetFloat("_Value", this.value);
+        //Save value and communicate to slider shader
+        currentColor.z = valueSlider.value;
+        //gameObject.GetComponent<CanvasRenderer>().GetMaterial().SetFloat("_Value", currentColor.z);
+        //Display new color
+        Color current = Color.HSVToRGB(currentColor.x, currentColor.y, currentColor.z);
+        displayedColor.GetComponent<CanvasRenderer>().SetColor(current);
     }
 }
