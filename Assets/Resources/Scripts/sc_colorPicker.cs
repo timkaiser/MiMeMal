9b0e249286f034a5665bb4e760ed0f2a25f6d210
Annalena <bloch@in.tum.de>
2019-07-01 17:35:57 +0200

    implemented pigment grid TODO: parse in json

--- Assets/Resources/Scripts/sc_colorPicker.cs
+++ Assets/Resources/Scripts/sc_colorPicker.cs
@@ -9,6 +9,8 @@ public class sc_colorPicker : MonoBehaviour
     public Slider valueSlider;
     public RawImage pointer;
     public RawImage displayedColor;
+    public Text pigmentName;
+    public Text pigmentText;
 
     private Vector3 currentColor = new Vector3(0, 0, 1);
 
@@ -34,24 +36,61 @@ public class sc_colorPicker : MonoBehaviour
         //If outside cicle neglect
         if (saturation > 1) return;
 
-        //Communicate color to slider shader
-        valueSlider.transform.Find("Color")
-                  .GetComponent<CanvasRenderer>().GetMaterial().SetVector("_HSVColor",
-                  new Vector4(hue, saturation, currentColor.z, 1));
         //Move pointer
         pointer.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y);
+
         //Set currently displayed color
         currentColor = new Vector3(hue, saturation, currentColor.z);
         Color current = Color.HSVToRGB(hue, saturation, currentColor.z);
         displayedColor.GetComponent<CanvasRenderer>().SetColor(current);
+
+        //Communicate color to slider shader
+        valueSlider.transform.Find("Color")
+                  .GetComponent<CanvasRenderer>().GetMaterial().SetVector("_HSVColor",
+                  new Vector4(hue, saturation, currentColor.z, 1));
+
+        //Set Info text invisible
+        pigmentName.text = "";
+        pigmentText.text = "";
     }
     public void OnValueSliderChanged()
     {
-        //Save value and communicate to slider shader
+        //Save value and communicate to circle shader
         currentColor.z = valueSlider.value;
         //gameObject.GetComponent<CanvasRenderer>().GetMaterial().SetFloat("_Value", currentColor.z);
+
         //Display new color
         Color current = Color.HSVToRGB(currentColor.x, currentColor.y, currentColor.z);
         displayedColor.GetComponent<CanvasRenderer>().SetColor(current);
+
+        //Set Info text invisible
+        pigmentName.text = "";
+        pigmentText.text = "";
     }
+
+    public void setColor(Color c)
+    {
+        float hue, saturation, value;
+        Color.RGBToHSV(c, out hue, out saturation, out value);
+
+        //Communicate color to slider
+        valueSlider.transform.Find("Color")
+                  .GetComponent<CanvasRenderer>().GetMaterial().SetVector("_HSVColor",
+                  new Vector4(hue, saturation, value, 1));
+        valueSlider.SetValueWithoutNotify(value);
+
+        //Set currently displayed color
+        currentColor = new Vector3(hue, saturation, value);
+        displayedColor.GetComponent<CanvasRenderer>().SetColor(c);
+
+        //Move pointer by computing world Position
+        float phi = (hue - 0.5f) * 2 * Mathf.PI;
+        float x = saturation * Mathf.Cos(phi);
+        float y = saturation * Mathf.Sin(phi);
+        RectTransform boundingRect = GetComponent<RectTransform>();
+        float worldX = (x * boundingRect.rect.size.x) / 2 + boundingRect.position.x;
+        float worldY = (y * boundingRect.rect.size.y) / 2 + boundingRect.position.y;
+        pointer.transform.position = new Vector3(worldX, worldY);
+    }
+
 }
--- Assets/Resources/Scripts/sc_pigments.cs
+++ Assets/Resources/Scripts/sc_pigments.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 public struct Pigment
 {
-    string name;
-    Color color;
-    string description;
+    public string name;
+    public Color color;
+    public string description;
 
     public Pigment(string name, Color color, string description)
     {
@@ -18,7 +20,12 @@ public struct Pigment
 public class sc_pigments : MonoBehaviour
 {
     public GameObject buttonPrefab;
-    
+    public sc_colorPicker colorPicker;
+    public Text pigmentName;
+    public Text pigmentText;
+
+    public int numberOfPigmentsVertical;
+    public int numberOfPigmentsHorizontal;
 
     private Pigment[] pigments;
 
@@ -28,21 +35,56 @@ public class sc_pigments : MonoBehaviour
         int numberOfPigments = 3;
         pigments = new Pigment[numberOfPigments];
         Pigment white = new Pigment("White", Color.white, "White is beautiful.");
-        Pigment black = new Pigment("White", Color.black, "Black is beautiful.");
-        Pigment red = new Pigment("White", Color.red, "Red is beautiful.");
+        Pigment black = new Pigment("Black", Color.black, "Black is beautiful.");
+        Pigment red = new Pigment("Red", Color.red, "Red is beautiful.");
         pigments[0] = white; pigments[1] = black; pigments[2] = red;
 
-        for(int i = 0; i < numberOfPigments; i++)
+        //Add pigment buttons as regular grid
+        int intervalX = (int)GetComponent<RectTransform>().rect.width / numberOfPigmentsVertical;
+        int intervalY = -(int)GetComponent<RectTransform>().rect.height / numberOfPigmentsHorizontal;
+        int offsetX = intervalX;
+        int offsetY = 2*intervalY;
+
+        for (int j = 0; j < numberOfPigmentsHorizontal; j++)
         {
-            int pigmentID = Instantiate(buttonPrefab, transform).GetInstanceID();
-            //GameObject[] instArr = GameObject.Find
-            //pigment.transform.position = new Vector3(i*39, -84, 0);
+            if ((j * numberOfPigmentsHorizontal) >= numberOfPigments) break;
+
+            for (int i = 0; i < numberOfPigmentsVertical; i++)
+            {
+                int IDX = j * numberOfPigmentsVertical + i;
+                if (IDX >= numberOfPigments) break;
+
+                GameObject o = Instantiate(buttonPrefab, transform);
+                Button b = o.transform.Find("Button").GetComponent<Button>();
+                //Move to correct position
+                o.transform.Translate(new Vector3(offsetX + intervalX * i, offsetY + intervalY * j, 0));
+                //Colorize
+                var colors = b.colors;
+                colors.normalColor = pigments[IDX].color;
+                colors.highlightedColor = pigments[IDX].color;
+                colors.pressedColor = pigments[IDX].color;
+                colors.selectedColor = pigments[IDX].color;
+                b.colors = colors;
+                //Set on click listener
+                b.onClick.AddListener(delegate () { pigmentSelected(pigments[IDX]); });
+            }
         }
+
+        //Set default text
+        pigmentName.text = pigments[0].name;
+        pigmentText.text = pigments[0].description;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void pigmentSelected(Pigment p)
+    {
+        colorPicker.setColor(p.color);
+        pigmentName.text = p.name;
+        pigmentText.text = p.description;
     }
 }
