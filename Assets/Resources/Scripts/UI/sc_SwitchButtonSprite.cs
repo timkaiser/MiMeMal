0cc18d632e8a8733616867c9ffb605225da0d00c
Jonathan <jjjooonnn@gmail.com>
2019-07-07 20:12:22 +0200

    Button Sprites
    Upgraded buttons with sprites

---
+++ Assets/Resources/Scripts/UI/sc_SwitchButtonSprite.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sc_SwitchButtonSprite : MonoBehaviour
+{
+    public GameObject sprite_1;
+    public GameObject sprite_2;
+
+    public void SwitchSprite() {
+        if(sprite_1.activeSelf && !sprite_2.activeSelf){
+            sprite_1.SetActive(false);
+            sprite_2.SetActive(true);
+            return;
+        }
+        sprite_2.SetActive(false);
+        sprite_1.SetActive(true);
+    }
+}
