88eeb204f33d268feea64e3deb782a8ea1f253ad
Annalena <bloch@in.tum.de>
2019-07-19 11:20:50 +0200

    moved back button in color canvas, cross in colorselection now moving

---
+++ AndroidApp/Assets/Resources/Scripts/ColorPicker/sc_color_picker_move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class sc_color_picker_move : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool isClicked = false;
+    public sc_color_picker color_Picker;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isClicked)
+        {
+            color_Picker.OnColorPickerClicked();
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isClicked = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        isClicked = false;
+    }
+}
