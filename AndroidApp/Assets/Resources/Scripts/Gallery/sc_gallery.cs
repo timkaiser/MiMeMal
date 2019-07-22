5893824f2ed03cf23d308f1e20db8777a5139912
Jonathan <jjjooonnn@gmail.com>
2019-07-22 17:41:24 +0200

    Auto browse
    Enabled auto browsing, set to a timer of 30 seconds, resets when:
    arrows are used
    onEnable of gallery canvas

--- AndroidApp/Assets/Resources/Scripts/Gallery/sc_gallery.cs
+++ AndroidApp/Assets/Resources/Scripts/Gallery/sc_gallery.cs
@@ -6,20 +6,25 @@ using UnityEngine;
 public class sc_gallery : MonoBehaviour
 {
     private sc_swipe_draggable rotation_script;
+    private sc_gallery_ui galleryUIScript;
 
     // Start is called before the first frame update
     void Start()
     {
         rotation_script = GameObject.FindGameObjectWithTag("paintable").GetComponent<sc_swipe_draggable>();
+        galleryUIScript = GameObject.FindGameObjectWithTag("script_manager").GetComponent<sc_gallery_ui>();
     }
 
     private void OnEnable()
     {
         try { rotation_script.Unlock(); } catch (Exception) { }
+        //restart the auto browse
+        galleryUIScript.restartAutoBrowse();
     }
 
     private void OnDisable()
     {
         try { rotation_script.Lock(); } catch (Exception) { }
     }
+
 }
